3efcddacf02caba6565af06f363769f291b8b807
gosharplite <tony.hsu@droi.com>
2015-12-09 18:32:38 +0800

    Remove root local flag.

--- cmd/root.go
+++ cmd/root.go
@@ -33,7 +33,7 @@ var RootCmd = &cobra.Command{
 Find more information at https://github.com/gosharplite/cluster.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	//	Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -51,11 +51,11 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports Persistent Flags, which, if defined here,
 	// will be global for your application.
-
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cluster.yaml)")
+
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
